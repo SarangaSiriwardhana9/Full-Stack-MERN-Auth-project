d2f35ce6eb112b984b704c64dab18607e81524c0
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-01-27 01:47:22 +0530

    create upload image functionality to image

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,52 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePresent, setImagePresent] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + "-" + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePresent(Math.round(progress));
+      },
+      // eslint-disable-next-line no-unused-vars
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, profilePicture: downloadURL });
+        });
+      }
+    );
+  };
 
   return (
     <div className='flex justify-center items-center h-screen bg-gray-100'>
@@ -13,11 +56,42 @@ export default function Profile() {
         </h1>
         <form className='mb-8'>
           <div className='flex justify-center items-center mb-6'>
-            <img
-              src={currentUser.profilePicture}
-              alt='profile'
-              className='rounded-full w-24 h-24 object-cover'
+            {/* Firebase Storage rules
+              allow read;
+              allow write: if
+              request.resource.size<2*1024*1024 &&
+              request.resource.contentType.matches('image/.*')
+            */}
+            <input
+              type='file'
+              ref={fileRef}
+              accept='image/*'
+              style={{ display: "none" }}
+              onChange={(e) => setImage(e.target.files[0])}
             />
+            <img
+              src={formData.profilePicture || currentUser.profilePicture}
+              alt='profile'
+              className='rounded-full w-24 h-24 cursor-pointer object-cover'
+              onClick={() => fileRef.current.click()}
+            />
+          </div>
+          <div className='mb-4'>
+            <p className='text-sm text-center self-center'>
+              {imageError ? (
+                <span className='text-red-700'>
+                  Error uploading image (file size must be less than 2 MB)
+                </span>
+              ) : imagePresent > 0 && imagePresent < 100 ? (
+                <span className='text-green-700'>{`Uploading: ${imagePresent} %`}</span>
+              ) : imagePresent === 100 ? (
+                <span className='text-green-700'>
+                  Image uploaded successfully
+                </span>
+              ) : (
+                ""
+              )}
+            </p>
           </div>
           <div className='mb-4'>
             <input
