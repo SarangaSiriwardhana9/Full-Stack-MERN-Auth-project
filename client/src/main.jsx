4144e3cb0cd6ce2e98b4dd715a201c9faed77824
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-01-26 15:12:36 +0530

    add redux persist for signin

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>
 );
